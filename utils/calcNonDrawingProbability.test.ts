736abb6bb2250ad56706f897425d1e6d07a8d895
yu-ko-ba <59269129+yu-ko-ba@users.noreply.github.com>
2023-07-17 17:04:46 +0900

    メイン画面を作成しました。

--- utils/calcNonDrawingProbability.test.ts
+++ utils/calcNonDrawingProbability.test.ts
@@ -6,3 +6,11 @@ test("300連して0.75%を一度も引けない確率の計算", () => {
   console.log(`約 ${result * 100} %`);
   expect(result).toBe(ok);
 });
+
+test("排出率が0%の場合", () => {
+  console.log(`排出率が0%の場合：${calcNonDrawingProbability(0, 100)}`);
+});
+
+test("試行回数が0回の場合", () => {
+  console.log(`試行回数が0回の場合：${calcNonDrawingProbability(1, 0)}`);
+});
--- utils/calcNonDrawingProbability.ts
+++ utils/calcNonDrawingProbability.ts
@@ -1,3 +1,4 @@
 export const calcNonDrawingProbability = (odds: number, times: number) => {
+  // 排出率や試行回数が0より小さい値だった場合の確率をご存じの方がいらっしゃったらissueやTwitterで教えてくださると幸いです。
   return (1 - odds) ** times;
 };
