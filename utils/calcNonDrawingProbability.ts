ed941870202c0c0df383e1b11123fe533b825646
yu-ko-ba <59269129+yu-ko-ba@users.noreply.github.com>
2024-09-23 21:22:56 +0900

    angular initial commit

--- utils/calcNonDrawingProbability.ts
+++
@@ -1,4 +0,0 @@
-export const calcNonDrawingProbability = (odds: number, times: number) => {
-  // 排出率や試行回数が0より小さい値だった場合の確率をご存じの方がいらっしゃったらissueやTwitterで教えてくださると幸いです。
-  return (1 - odds) ** times;
-};
