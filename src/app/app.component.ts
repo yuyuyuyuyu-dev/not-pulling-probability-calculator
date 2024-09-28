410f43d9066143dc89aea6fc0f95d4d68424ff9a
yu-ko-ba <59269129+yu-ko-ba@users.noreply.github.com>
2024-09-28 11:27:05 +0900

    feature: non-drawing-probability-calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MyAppbarComponent } from './my-appbar/my-appbar.component';
+import { NgxMatMySimpleAppbarComponent } from '@yu-ko-ba/ngx-mat-my-simple-appbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    MyAppbarComponent,
+    NgxMatMySimpleAppbarComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'non-drawing-probability-calculator';
+  title = '引けない確率の計算';
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  protected readonly odds = signal(0);
+  protected readonly times = signal(0);
 
+  protected onOddsInput(event: Event) {
+    this.odds.set(Number((event.target as HTMLInputElement).value));
+  }
+
+  protected onTimesInput(event: Event) {
+    this.times.set(Number((event.target as HTMLInputElement).value));
+  }
+
+  protected calcNonDrawingProbability(odds: number, times: number) {
+    return ((1 - (odds / 100)) ** times) * 100;
+  }
 }
