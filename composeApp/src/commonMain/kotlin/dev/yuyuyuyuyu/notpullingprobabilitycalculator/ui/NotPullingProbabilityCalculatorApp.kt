59b388701b30f9924daf48e2f3f81ac79d37d2c1
yu <email@yuyuyuyuyu.dev>
2025-07-28 18:19:19 +0900

    feat(composeApp.ui): add top app bar

--- composeApp/src/commonMain/kotlin/dev/yuyuyuyuyu/notpullingprobabilitycalculator/ui/NotPullingProbabilityCalculatorApp.kt
+++ composeApp/src/commonMain/kotlin/dev/yuyuyuyuyu/notpullingprobabilitycalculator/ui/NotPullingProbabilityCalculatorApp.kt
@@ -1,6 +1,10 @@
 package dev.yuyuyuyuyu.notpullingprobabilitycalculator.ui
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalUriHandler
 import com.slack.circuit.backstack.rememberSaveableBackStack
 import com.slack.circuit.foundation.CircuitCompositionLocals
 import com.slack.circuit.foundation.NavigableCircuitContent
@@ -8,6 +12,8 @@ import com.slack.circuit.foundation.rememberCircuitNavigator
 import dev.yuyuyuyuyu.mymaterialtheme.MyMaterialTheme
 import dev.yuyuyuyuyu.notpullingprobabilitycalculator.di.notPullingProbabilityCalculatorAppModule
 import dev.yuyuyuyuyu.notpullingprobabilitycalculator.ui.notPullingProbabilityCalculator.NotPullingProbabilityCalculatorScreenImpl
+import dev.yuyuyuyuyu.notpullingprobabilitycalculator.ui.openSourceLicenseList.OpenSourceLicenseListScreen
+import dev.yuyuyuyuyu.simpleTopAppBar.SimpleTopAppBar
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.KoinApplication
 import org.koin.compose.koinInject
@@ -18,6 +24,8 @@ fun NotPullingProbabilityCalculatorApp() {
     val backStack = rememberSaveableBackStack(root = NotPullingProbabilityCalculatorScreenImpl)
     val navigator = rememberCircuitNavigator(backStack) {}
 
+    val uriHandler = LocalUriHandler.current
+
     KoinApplication(
         application = {
             printLogger()
@@ -25,8 +33,29 @@ fun NotPullingProbabilityCalculatorApp() {
         },
     ) {
         MyMaterialTheme {
-            CircuitCompositionLocals(circuit = koinInject()) {
-                NavigableCircuitContent(navigator, backStack)
+            Scaffold(
+                topBar = {
+                    val currentScreen = backStack.topRecord?.screen
+
+                    SimpleTopAppBar(
+                        title = when (currentScreen) {
+                            is OpenSourceLicenseListScreen -> "オープンソース ライセンス"
+                            else -> "引けない確率の計算"
+                        },
+                        navigateBackIsPossible = backStack.size > 1,
+                        onNavigateBackButtonClick = { navigator.pop() },
+                        onOpenSourceLicensesButtonClick = {
+                            navigator.goTo(OpenSourceLicenseListScreen)
+                        },
+                        onSourceCodeButtonClick = {
+                            uriHandler.openUri("https://github.com/yuyuyuyuyu-dev/not-pulling-probability-calculator")
+                        }
+                    )
+                }
+            ) { innerPadding ->
+                CircuitCompositionLocals(circuit = koinInject()) {
+                    NavigableCircuitContent(navigator, backStack, Modifier.padding(innerPadding))
+                }
             }
         }
     }
