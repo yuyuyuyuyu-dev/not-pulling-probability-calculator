57b7fe3c37bb65b1907db8698e04ae9f73334579
yu <email@yuyuyuyuyu.dev>
2025-08-02 12:51:11 +0900

    feat(ui): implement UX
    - apply number keyboard type

--- shared/src/commonMain/kotlin/dev/yuyuyuyuyu/notpullingprobabilitycalculator/shared/ui/notPullingProbabilityCalculator/NotPullingProbabilityCalculator.kt
+++ shared/src/commonMain/kotlin/dev/yuyuyuyuyu/notpullingprobabilitycalculator/shared/ui/notPullingProbabilityCalculator/NotPullingProbabilityCalculator.kt
@@ -14,66 +14,72 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun NotPullingProbabilityCalculator(
     state: NotPullingProbabilityCalculatorScreen.State,
     modifier: Modifier = Modifier,
-) = Column(
+) = Box(
     modifier = modifier.fillMaxSize(),
-    verticalArrangement = Arrangement.Center,
-    horizontalAlignment = Alignment.CenterHorizontally,
+    contentAlignment = Alignment.Center,
 ) {
-    var odds by rememberSaveable { mutableStateOf("") }
-    var numberOfTrials by rememberSaveable { mutableStateOf("") }
+    Column(
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+    ) {
+        var odds by rememberSaveable { mutableStateOf("") }
+        var numberOfTrials by rememberSaveable { mutableStateOf("") }
 
-    val focusManager = LocalFocusManager.current
+        val focusManager = LocalFocusManager.current
 
-    TextField(
-        value = odds,
-        onValueChange = {
-            odds = it
-            state.eventSink(
-                NotPullingProbabilityCalculatorScreen.Event.ChangeInputValue(
-                    odds = it.toDoubleOrNull() ?: 0.0,
-                    numberOfTrials = numberOfTrials.toIntOrNull() ?: 0,
+        TextField(
+            value = odds,
+            onValueChange = {
+                odds = it
+                state.eventSink(
+                    NotPullingProbabilityCalculatorScreen.Event.ChangeInputValue(
+                        odds = it.toDoubleOrNull() ?: 0.0,
+                        numberOfTrials = numberOfTrials.toIntOrNull() ?: 0,
+                    )
                 )
-            )
-        },
-        label = { Text("排出率（単位：%）") },
-        keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
-        maxLines = 1,
-    )
+            },
+            label = { Text("排出率（単位：%）") },
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next,
+            ),
+            maxLines = 1,
+        )
 
-    Spacer(Modifier.height(16.dp))
-
-    TextField(
-        value = numberOfTrials,
-        onValueChange = {
-            numberOfTrials = it
-            state.eventSink(
-                NotPullingProbabilityCalculatorScreen.Event.ChangeInputValue(
-                    odds = odds.toDoubleOrNull() ?: 0.0,
-                    numberOfTrials = it.toIntOrNull() ?: 0,
+        TextField(
+            value = numberOfTrials,
+            onValueChange = {
+                numberOfTrials = it
+                state.eventSink(
+                    NotPullingProbabilityCalculatorScreen.Event.ChangeInputValue(
+                        odds = odds.toDoubleOrNull() ?: 0.0,
+                        numberOfTrials = it.toIntOrNull() ?: 0,
+                    )
                 )
-            )
-        },
-        label = { Text("試行回数（単位：回）") },
-        keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
-        keyboardActions = KeyboardActions(onDone = { focusManager.clearFocus() }),
-        maxLines = 1,
-    )
+            },
+            label = { Text("試行回数（単位：回）") },
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done,
+            ),
+            keyboardActions = KeyboardActions(onDone = { focusManager.clearFocus() }),
+            maxLines = 1,
+        )
 
-    Spacer(Modifier.height(48.dp))
+        Column {
+            Text("一度も引けない確率")
+            Text("約 ${state.notPullingProbability} %")
 
-    Column {
-        Text("一度も引けない確率")
-        Text("約 ${state.notPullingProbability} %")
+            Spacer(Modifier.height(16.dp))
 
-        Spacer(Modifier.height(16.dp))
-
-        Text("少なくとも1回は引ける確率")
-        Text("約 ${state.pullingProbability} %")
+            Text("少なくとも1回は引ける確率")
+            Text("約 ${state.pullingProbability} %")
+        }
     }
 }
